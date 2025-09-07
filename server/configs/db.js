40919b6e9dd7349024b7ecf34ad2029643222794
ajharul <mdajharulislam327@gmail.com>
2025-09-07 22:33:46 +0600

    dc

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,16 +1,11 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  try {
-    mongoose.connection.on('connected', () => {
-      console.log('MongoDB connected successfully');
-    });
-   await mongoose.connect(`${process.env.MONGODB_URI}/GREEN_CART`);
-
-  } catch (error) {
-    console.error(error.message);
-   
-  }
+const connectDB = async ()=>{
+    try {
+        mongoose.connection.on('connected',()=>console.log("database connected"))
+      await mongoose.connect(`${process.env.MONGODB_URI}/quickblog`)  
+    } catch (error) {
+        console.log(error.message)
+    }
 }
-
 export default connectDB;
