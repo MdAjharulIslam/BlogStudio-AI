9d57d81cdcb9566c0115caf6cf2fcfc2f887e150
ajharul <mdajharulislam327@gmail.com>
2025-09-07 22:27:37 +0600

    df

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,20 +2,15 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    mongoose.connection.on('connected', () => {
+      console.log('MongoDB connected successfully');
     });
+   await mongoose.connect(`${process.env.MONGODB_URI}/GREEN_CART`);
 
-    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
-
-    mongoose.connection.on("error", (err) => {
-      console.error("MongoDB connection error:", err);
-    });
   } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1); // stop server if DB cannot connect
+    console.error(error.message);
+   
   }
-};
+}
 
 export default connectDB;
